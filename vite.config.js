2da11706aa06ba68c6bb85d9a4e52a998f04673c
glaures <guido@laures.de>
2024-07-03 00:43:39 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/forestshuffle/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

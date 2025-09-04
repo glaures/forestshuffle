8155f2b9cb2e4b43e1d80d5443ae7b9098d690c5
Guido Laures <guido@laures.de>
2025-09-04 15:29:01 +0200

    Updates

--- assets/bootstrap-Q_Pa-GeZ.js
+++ assets/bootstrap-Q_Pa-GeZ.js
@@ -1,4 +1,4 @@
-import{P as de,c as fe}from"./@popperjs-WhmJkuoZ.js";/*!
+import{P as de,c as fe}from"./@popperjs-BEKk9TLb.js";/*!
   * Bootstrap v5.3.3 (https://getbootstrap.com/)
   * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
